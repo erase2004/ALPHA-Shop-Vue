cd4e18add20a87021af947f50fada1e114c72863
erase2004 <erase2004@gmail.com>
2022-02-08 16:13:38 +0800

    1. re-commit with missing change

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,8 +3,6 @@
     <!-- register -->
     <section
       class="register-container col col-lg-6 col-sm-12"
-      data-phase="1"
-      data-total-price="0"
     >
       <!-- register-title -->
       <h2 class="register-title col col-12">
@@ -17,21 +15,31 @@
       <!-- register-form -->
       <section class="form-container col col-12">
         <!-- address phase -->
-        <AddressForm v-show="page === 'address'" />
+        <AddressForm
+          v-show="page === 'address'"
+          @address-form-modified="addressFormData = $event"
+        />
         <!-- shipping phase -->
         <ShippingForm
           v-show="page === 'shipping'"
           @shipping-changed="shippingFee = $event"
         />
-        <!-- credit-card phase -->
-        <PaymentForm v-show="page === 'payment'" />
+        <!-- payment phase -->
+        <PaymentForm
+          v-show="page === 'payment'"
+          @payment-form-modified="paymentFormData = $event"
+        />
       </section>
     </section>
     <Cart
       :shipping-fee="shippingFee"
       @total-changed="priceTotal = $event"
     />
-    <ProgressControl :page="page" />
+    <ProgressControl
+      :page="page"
+      @modal-opened="$emit('modal-opened')"
+    />
+    <span class="hidden">{{ hiddenOutput }}</span>
   </div>
 </template>
 
@@ -55,7 +63,27 @@ export default {
     return {
       page: 'address',
       shippingFee: 0,
-      priceTotal: 0
+      priceTotal: 0,
+      addressFormData: {},
+      paymentFormData: {}
+    }
+  },
+  computed: {
+    hiddenOutput () {
+      const wholeData = Object.assign({},
+        this.addressFormData,
+        { shippingFee: this.shippingFee },
+        this.paymentFormData,
+        { totalPrice: this.priceTotal }
+      )
+
+      this.$emit('whole-data-modified', wholeData)
+      return ''
+    }
+  },
+  watch: {
+    '$route.name': function (newValue) {
+      this.page = newValue
     }
   },
   created () {
